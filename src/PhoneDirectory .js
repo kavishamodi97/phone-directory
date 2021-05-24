81eb4e0871ce2b20dbf5ebcf66368024d2c87349
kavishamodi97 <kavishamodi971@gmail.com>
2021-05-24 20:25:05 +0530

    Added Delete Subscriber

--- src/PhoneDirectory .js
+++ src/PhoneDirectory .js
@@ -33,11 +33,25 @@ class PhoneDirectory extends Component {
         this.setState({ subscribersList: subscribersList });
     }
 
+
+    deleteSubscriberHandler = (subscriberId) => {
+        let subscribersList = this.state.subscribersList;
+        let subscriberIndex = 0;
+        subscribersList.forEach(function (subscriber, index) {
+            if (subscriber.id == subscriberId) {
+                subscriberIndex = index;
+
+            }
+        }, this);
+        let newScribers = subscribersList;
+        newScribers.splice(subscriberIndex, 1);
+        this.setState({ subscribers: newScribers });
+    }
     render() {
         return (
             <Router>
                 <div>
-                    <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} />} />
+                    <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} deleteSubscriberHandler={this.deleteSubscriberHandler} />} />
                     <Route exact path="/add" render={({ history }, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} />} />
                 </div>
             </Router>
